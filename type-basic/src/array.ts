72f2a2e2aa036aa093f57b7f4b10bf38f8ad0ebb
rzkfyn <rzkfyn@gmail.com>
2022-02-15 19:14:58 +0700

    Belajar type array

---
+++ type-basic/src/array.ts
@@ -0,0 +1,15 @@
+// Juga sama seperti java, kita bisa menentukan array yang kita deklarasikan untuk menampung data berjenis apa
+
+/* Array Menampung Data String */
+let arrstr: string[] = ["satu", "dua", "tiga"];
+// arrstr = ["satu", 2, true] // error
+
+/* Array Menampung Data String */
+let arrnum: number[];
+// arrnum = ["satu",, 2, 3, {nama : "nama"}]; // error
+arrnum = [1,2,3];
+
+/* Array Untuk Menampung Data Boolean */
+let arrbool: boolean[];
+// arrbool = ["true", false, ["Hello", "World"]]; // error
+arrbool = [true, false, 3 < 5];
