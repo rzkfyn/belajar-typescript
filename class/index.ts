8e56a1626b45176ed5dbdfd584840f1b9fb71bd9
rzkfyn <rzkfyn@gmail.com>
2022-02-26 10:48:55 +0700

    Belajar setter dan getter

--- class/index.ts
+++ class/index.ts
@@ -81,39 +81,111 @@ private : Hanya bisa di akses didalam class tersebut
 // /* Karena Admin turunan dari User, Admin bisa  menggunakan method milik User */
 
 // Super Contructor
+// class User{
+//     private name: string;
+//     private age: number;
+
+//     public constructor(name: string, age: number){
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     public setName(name: string): void{
+//         this.name = name;
+//     }
+
+//     public getName(): string{
+//         return this.name;
+//     }
+// }
+
+// class Admin extends User{
+//     public read: boolean = true;
+//     public write: boolean = true;
+//     private phone: string;
+
+//     /* 
+//     Jika kita membuat constructor pada sebuah class turunan tapi pada class parentnya juga terdapat sebuah constructor method,
+//     maka constructor pada class parentnya juga harus diisi menggunakan method super() untuk mengisi atribut yang diperlukan
+//     contructor method class parentnya.
+//     */
+
+//     /* Atribut pada class parentnya juga dituliskan sebagai parameter */
+//     public constructor(phone: string, name: string, age: number){
+//         /* Dan diisi disini dengan method super() */
+//         super(name, age);
+//         this.phone = phone;
+//     }
+
+//     public getRole(): {read: boolean, write: boolean}{
+//         return {
+//             read: this.read,
+//             write: this.write
+//         };
+//     }
+// }
+
+// /* Karena pada admin terdapat atribut baru maka kita juga harus mengirim argumen yang dijadikan parameternya */
+// const admin = new Admin('0812345678910', 'Sofyan', 17);
+// console.log(admin.getRole());
+
+// setter dan getter
+/*
+Setter merupakan sebuah fungsi yang biasanya digunakan untuk mengatur atau menentukan nilai dari atribut sebuah kelas
+tanpa mengakses atribut tersebut secara langsung. Dan getter merupakan sebuah fungsi untuk mengambil atau membaca atribut 
+dari class secara tidak mengakses atribut tersebut secara langsung.
+*/
 class User{
     private name: string;
     private age: number;
+    private _email: string;
 
-    public constructor(name: string, age: number){
+    public constructor(name: string, age: number, email: string){
         this.name = name;
         this.age = age;
+        this._email = email;
     }
 
-    public setName(name: string): void{
-        this.name = name;
+    // setter dan getter pada bahasa lain
+    public setName(name: string){
+        this.name= name;
     }
 
     public getName(): string{
         return this.name;
     }
+    // setter dan getter pada bahasa lain
+    /*
+    Umumnya begitulah setter dan getter pada bahasa pemrogaraman oop lain seperti java dan php
+    tapi, pada ts juga js, fungsi setter dan getter diawali dengan keyword set atau get.
+    contoh: 
+    */
+
+    public set email(value: string){
+        const characters = value.split('');
+        let a = 0;
+        characters.forEach(char => {
+            if(char == '@') a += 1;
+            if(char == ' ') throw new Error('Email tidak boleh menggukanan spasi!');
+        });
+        if(a < 1) throw new Error('Email tidak valid!');
+        this._email = value;
+    }
+
+    public get email(): string{
+        return this._email;
+    }
 }
 
+
 class Admin extends User{
+
     public read: boolean = true;
     public write: boolean = true;
     private phone: string;
 
-    /* 
-    Jika kita membuat constructor pada sebuah class turunan tapi pada class parentnya juga terdapat sebuah constructor method,
-    maka constructor pada class parentnya juga harus diisi menggunakan method super() untuk mengisi atribut yang diperlukan
-    contructor method class parentnya.
-    */
-
-    /* Atribut pada class parentnya juga dituliskan sebagai parameter */
-    public constructor(phone: string, name: string, age: number){
-        /* Dan diisi disini dengan method super() */
-        super(name, age);
+    public constructor(phone: string, name: string, age: number, email: string){
+        super(name, age, email);
         this.phone = phone;
     }
 
@@ -125,6 +197,14 @@ class Admin extends User{
     }
 }
 
-/* Karena pada admin terdapat atribut baru maka kita juga harus mengirim argumen yang dijadikan parameternya */
-const admin = new Admin('0812345678910', 'Sofyan', 17);
-console.log(admin.getRole());
+const admin = new Admin('0812345678910', 'Sofyan', 17, 'rzkfyn@gmail.com');
+/*
+fungsi setter dan getter pada js dan ts cukup unik karena saat memanggil fungsi tersebut tidak
+perlu menggunakan kurung '()' jadi kesannya seperti mengakses atribut secara langsung padahal
+sebernarnya kita memanggil sebuah fungsi dan menurut saia ini terasa cukup aneh wkwk
+*/
+// menggunakan fungsi setter
+admin.email = 'rzkfyn@gmail.com'
+
+// memanggil fungsi getter
+console.log(admin.email);
