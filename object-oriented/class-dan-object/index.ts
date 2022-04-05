f2265792ca8641623cff6a959648830cd90c051f
rzkfyn <rzkfyn@gmail.com>
2022-04-05 18:38:07 +0700

    Pelajaran baru : OOP Typescript | belajar class dan object

---
+++ object-oriented/class-dan-object/index.ts
@@ -0,0 +1,10 @@
+import Hewan from "./src/Hewan";
+
+/* Object */
+// Merupakan sebuah instansiai dari sebuah class yang mana akan memiliki sifat dan prilaku
+// yang telah di buat didalam class
+const kucing = new Hewan();
+const anjing = new Hewan();
+
+console.log(typeof kucing);
+console.log(typeof Hewan);
---
+++ object-oriented/class-dan-object/src/Hewan.ts
@@ -0,0 +1,6 @@
+/* Class */
+// Class merupakan sebuah kerangka atau blue print untuk membuat sebuah object
+// Didalam class dimuat perilaku dan sifat yang disebut properti dan method
+export default class Hewan {
+
+}
