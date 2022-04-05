08df94dfe7d37f3bb6740ce163f962dab2444761
rzkfyn <rzkfyn@gmail.com>
2022-04-05 19:14:26 +0700

    Belajar constructor

---
+++ object-oriented/constructor/index.ts
@@ -0,0 +1,6 @@
+import Hewan from "./src/Hewan";
+
+const kucing = new Hewan();
+// Tidak 'console.log' apapun tapi karena pada class hewan terdapat 
+// Contructor method yang console.log('Class di instansiasi') maka pada console akan
+// dicetak 'Class di instansiasi'
---
+++ object-oriented/constructor/src/Hewan.ts
@@ -0,0 +1,9 @@
+/* Constructor */
+// Merupakan sebuah method yang akan dipanggil pertama kali 
+// ketika sebuah kelas di instansiasi kepada object
+export default class Hewan {
+    // contoh 
+    public constructor() {
+        console.log(`Class di instansiasi`); // Ini akan membuat 'Class di instansiasi' di cetak ke console ketika class dipanggil (di instansiasi)
+    }
+}
